f3be1533c689ea666b4690090bd8378cf07cc4de
osvaldo <osvaldolazaroh@gmail.com>
2026-09-25 13:50:42 +0200

    fix: corretto condizione hackademy

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 for($i = 1; $i <= 100; $i++){
     
-    if($i % 3 == 0){
-        echo 'PHP' ."\n";
+    if($i % 3 == 0 && $i % 5 == 0){
+        echo 'HACKADEMY' ."\n";
     }elseif($i % 5 == 0){
         echo 'JAVASCRIPT'."\n";
-    }elseif($i % 3 == 0 && $i % 5 == 0){
-        echo 'HACKADEMY'."\n";
+    }elseif($i % 3 == 0){
+        echo 'PHP'."\n";
     }else{
         echo $i ."\n";
     }
